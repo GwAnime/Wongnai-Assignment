f702921c13312f16ccfaac18beab67312143e1b6
suphakid <gwanimem@gmail.com>
2023-02-08 16:01:36 +0700

    API gateway

--- packages/api-gateway/index.ts
+++ packages/api-gateway/index.ts
@@ -1,4 +1,5 @@
 import express, { Application } from "express";
+import fetch from "node-fetch";
 
 const app: Application = express();
 const port = 3001;
@@ -6,12 +7,53 @@ const port = 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.send("LINE MAN Wongnai Frontend Assignment"));
+app.get("/restaurant/:restaurantId", async (req, res) => {
+  try {
+    const { restaurantId } = req.params;
+    const result = await fetch(
+      `https://us-central1-wongnai-frontend-assignment.cloudfunctions.net/api/restaurants/${restaurantId}.json`
+    );
+    const data = await result.json();
+    res.send(data);
+  } catch (error) {
+    console.error(`Error occured: ${(error as Error).message}`);
+    res.status(500).send("Error Occurred");
+  }
+});
+
+app.get("/restaurant/:restaurantId/menus/:menuName/short", async (req, res) => {
+  try {
+    const { restaurantId, menuName } = req.params;
+    const result = await fetch(
+      `https://us-central1-wongnai-frontend-assignment.cloudfunctions.net/api/restaurants/${restaurantId}/menus/${menuName}/short.json`
+    );
+    const data = await result.json();
+    res.send(data);
+  } catch (error) {
+    console.error(`Error occured: ${(error as Error).message}`);
+    res.status(500).send("Error Occurred");
+  }
+});
+
+app.get("/restaurant/:restaurantId/menus/:menuName/full", async (req, res) => {
+  try {
+    const { restaurantId, menuName } = req.params;
+    const result = await fetch(
+      `https://us-central1-wongnai-frontend-assignment.cloudfunctions.net/api/restaurants/${restaurantId}/menus/${menuName}/full.json`
+    );
+    const data = await result.json();
+    res.send(data);
+  } catch (error) {
+    console.error(`Error occured: ${(error as Error).message}`);
+    res.status(500).send("Error Occurred");
+  }
+});
 
 try {
-	app.listen(port, (): void => {
-		console.log(`Connected successfully on port ${port}`);
-	});
+  app.listen(port, (): void => {
+    console.log(`Connected successfully on port ${port}`);
+  });
 } catch (error) {
-	console.error(`Error occured: ${(error as Error).message}`);
+  console.error(`Error occured: ${(error as Error).message}`);
 }
+
--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,15 +1,8 @@
 
-const fetchYo = () => {
-  fetch('http://localhost:3001/')
-    .then(response => response.json()) // turn to json file
-    .then(data => console.log(data)) // print data in json file format for easier
-}
-
 function App() {
   return (
     <div>
-      <button onClick={fetchYo}>Click Me</button>
-      <h1>Edit this app to complete LINE MAN Wongnai Frontend Assignment!</h1>
+      <h1>Web</h1>
     </div>
 
 
